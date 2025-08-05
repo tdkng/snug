3a02ede2e56c0dea8afce8c3b52958f78e42697a
tdkng <138352011+tdkng@users.noreply.github.com>
2025-08-04 20:30:34 -0700

    initial commit

---
+++ src/main/java/com/tdkng/snug/SnugApplication.java
@@ -0,0 +1,13 @@
+package com.tdkng.snug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdkng/snug/SnugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdkng.snug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
