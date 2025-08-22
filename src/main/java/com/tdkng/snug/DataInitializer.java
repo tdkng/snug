7e5e9195bab21ffbcfda9340929f832909dbc9a8
tdkng <138352011+tdkng@users.noreply.github.com>
2025-08-21 23:44:27 -0700

    Added data initialization

---
+++ src/main/java/com/tdkng/snug/DataInitializer.java
@@ -0,0 +1,44 @@
+package com.tdkng.snug;
+
+import java.util.ArrayList;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.tdkng.snug.model.AppUser;
+import com.tdkng.snug.model.Profile;
+import com.tdkng.snug.model.Review;
+import com.tdkng.snug.repository.AppUserRepository;
+import com.tdkng.snug.repository.ProfileRepository;
+import com.tdkng.snug.repository.ReviewRepository;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@AllArgsConstructor
+public class DataInitializer {
+    private final AppUserRepository appUserRepository;
+    private final ProfileRepository profileRepository;
+    private final ReviewRepository reviewRepository;
+
+    @Bean
+	public CommandLineRunner initialize() {
+		return (args -> {
+			AppUser user1 = new AppUser();
+
+            Profile profile1 = new Profile();
+            profile1.setAppUser(user1);
+
+			Review review1 = new Review();
+			review1.setReviewRating(4);
+			review1.setReviewComments("place is very pretty!");
+			review1.setAppUser(user1);
+            // user1.getReviews().add(review1);
+
+            appUserRepository.save(user1);
+            profileRepository.save(profile1);
+            reviewRepository.save(review1);
+		});
+	}
+}
--- src/main/java/com/tdkng/snug/model/Profile.java
+++ src/main/java/com/tdkng/snug/model/Profile.java
@@ -6,8 +6,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Profile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
