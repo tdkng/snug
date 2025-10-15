59648172c92cdd91f77986d5210e0e8b1e50e590
tdkng <138352011+tdkng@users.noreply.github.com>
2025-10-15 00:57:35 -0700

    home page redesign

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,20 +1,27 @@
 import { FaSearch } from "react-icons/fa";
+import home_bg from '../../assets/cafe_bg.jpg';
 
 const Home = () => {
     document.body.style.backgroundColor = 'var(--color-brown)';
     return (
-        <div>
-            <div className="flex items-center justify-center h-[calc(100vh-80px)]">
-                <ul class="space-y-4">
-                    <li><h1 className=" text-toffee text-5xl font-bold">
+        <div className="flex h-[calc(100vh-80px)]">
+            <div>
+                <img
+                    className="absolute -translate-y-[112px] h-[calc(100vh-80px)] w-full object-cover object-center -z-10 opacity-70"
+                    src={home_bg}
+                />
+            </div>
+            <div className="flex justify-center items-center w-full h-[720px]">
+                <ul className="space-y-4">
+                    <li><h1 className="text-white text-center text-5xl font-bold">
                         Find your next study spot here.
                     </h1></li>
                     <li>
                         <div className="relative w-2/3 max-w-xl mx-auto">
-                            <FaSearch className="absolute left-4 top-1/2 -translate-y-1/2 text-2xl text-brown opacity-70"/>
+                            <FaSearch className="absolute left-4 top-1/2 -translate-y-1/2 text-2xl text-dark-brown opacity-70"/>
                             <input
                                 type="text"
-                                className="w-full py-4 pl-12 pr-6 text-lg rounded-full shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 text-brown bg-white"
+                                className="w-full py-4 pl-12 pr-6 text-lg border-2 border-gray-300 rounded-full shadow-md focus:outline-none text-dark-brown bg-white"
                                 placeholder="San Francisco, CA"
                             />
                         </div>
--- frontend/src/components/shared/NavBar.jsx
+++ frontend/src/components/shared/NavBar.jsx
@@ -4,22 +4,22 @@ import { useLocation } from "react-router-dom";
 const NavBar = () => {
     const path = useLocation().pathname;
     return (
-        <div className="h-[80px] z-50 flex items-center sticky top-0 bg-toffee">
-            <div className="lg:px-16 sm:px-8 px-4 w-full flex justify-between">
-                <Link to="/" className="flex items-center text-2xl font-bold text-white">
-                    <p className="text-white">snug_app</p>
+        <div className="h-[80px] z-50 m-4 top-4 flex items-center sticky bg-white/30 backdrop-blur-sm shadow-md rounded-2xl">
+            <div className="lg:px-16 sm:px-8 px-4 w-full flex justify-between text-white">
+                <Link to="/" className="flex items-center text-2xl font-bold">
+                    <p>snug_app</p>
                 </Link>
                 <ul>
                     <li className="inline-block px-4">
                         <Link className={`${
-                            path === "/" ? "text-white font-bold" : "text-white"
+                            path === "/" ? "font-bold" : ""
                         }`} to="/">
                             Home
                         </Link>
                     </li>
                     <li className="inline-block px-4">
                         <Link className={`${
-                            path === "/query" ? "text-white font-bold" : "text-white"
+                            path === "/query" ? "font-bold" : ""
                         }`} to="/query">
                             Search
                         </Link>
@@ -33,14 +33,14 @@ const NavBar = () => {
                     </li> */}
                     <li className="inline-block px-4">
                         <Link className={`${
-                            path === "/contact" ? "text-white font-bold" : "text-white"
+                            path === "/contact" ? "font-bold" : ""
                         }`} to="/contact">
                             Contact
                         </Link>
                     </li>
                     <li className="inline-block px-4">
                         <Link className={`${
-                            path === "/login" ? "text-white font-bold" : "text-white"
+                            path === "/login" ? "font-bold" : ""
                         }`} to="/login">
                             Login
                         </Link>
