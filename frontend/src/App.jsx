7dff6fb36dc35bfba6c56ad1f1202df3e7d066db
tdkng <138352011+tdkng@users.noreply.github.com>
2025-11-04 20:02:02 -0800

    Revert "created sample profile page"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './pages/AboutPage.jsx';
 import Contact from './pages/ContactPage.jsx';
 import Home from './pages/HomePage.jsx';
-import LandingPage from './pages/LandingPage.jsx';
-import Login from './pages/LoginPage.jsx';
-import Profile from './pages/ProfilePage.jsx';
-import Signup from './pages/SignupPage.jsx';
 import Query from './pages/QueryPage.jsx';
+import Login from './pages/LoginPage.jsx';
+import Signup from './pages/SignupPage.jsx';
+import LandingPage from './pages/LandingPage.jsx';
 
 function App() {
   return (
@@ -20,7 +19,6 @@ function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/home' element={<Home />} />
-          <Route path='/profile' element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
